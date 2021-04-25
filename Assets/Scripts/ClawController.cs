e97f5b3f6c3939bbaeb061f3dcabcfb6ccc78473
Ebru Çakar <ebru_cakar_11@hotmail.com>
2021-04-25 16:16:26 +0300

    Initial commit

---
+++ Assets/Scripts/ClawController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClawController : MonoBehaviour
+{
+    [SerializeField] DynamicJoystick MyJoystick;
+
+    public float speed;
+
+    public void FixedUpdate()
+    {
+        Vector3 direction = Vector3.forward * MyJoystick.Vertical + Vector3.right * MyJoystick.Horizontal;
+        transform.position += direction * speed * Time.fixedDeltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Glacier"))
+        {
+            Debug.Log(other.name);
+            other.GetComponent<Collider>().enabled = false;
+
+        }
+    }
+}
---
+++ Assets/Scripts/Glacier.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glacier : MonoBehaviour
+{
+    [SerializeField] float LifeTime;
+    float timer;
+
+    private void Start()
+    {
+        timer = LifeTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Debug.Log("Öldü");
+        }
+        else if (timer < LifeTime / 2)
+        {
+
+        }
+    }
+}
