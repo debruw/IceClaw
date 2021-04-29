81e217545261e19fe8a746f82ac619e888684e0b
Ebru Çakar <ebru_cakar_11@hotmail.com>
2021-04-30 00:46:37 +0300

    Temel mekanik ayarlandı.

--- Assets/Scripts/ClawController.cs
+++ Assets/Scripts/ClawController.cs
@@ -6,51 +6,71 @@ public class ClawController : MonoBehaviour
 {
     [SerializeField] DynamicJoystick MyJoystick;
     public float speed;
-    public GameObject[] EmptySlots;
-    public GameObject GlacierParent;
-    int nextSlot = 1;
+
     Vector2 xClamp, zClamp;
     Vector3 startPos;
+    [SerializeField] GameObject Arm1, Arm2, Arm3, Arm4, Body;
+    float bodyStartRotationX;
 
-    private void Start()
+    private void Awake()
     {
-        xClamp = new Vector2(transform.localPosition.x - 2, transform.localPosition.x + 2);
-        zClamp = new Vector2(transform.localPosition.z - 1, transform.localPosition.z + 2);
+        xClamp = new Vector2(transform.localPosition.x - 5, transform.localPosition.x + 5);
+        zClamp = new Vector2(transform.localPosition.z - 5, transform.localPosition.z + 5);
+        bodyStartRotationX = Body.transform.localEulerAngles.x;
     }
 
-    public void FixedUpdate()
+    private void Update()
     {
         Vector3 direction = Vector3.forward * MyJoystick.Vertical + Vector3.right * MyJoystick.Horizontal;
         transform.position += direction * speed * Time.fixedDeltaTime;
+
         transform.localPosition = new Vector3(Mathf.Clamp(transform.localPosition.x, xClamp.x, xClamp.y), transform.localPosition.y, Mathf.Clamp(transform.localPosition.z, zClamp.x, zClamp.y));
+        Body.transform.localEulerAngles = new Vector3(bodyStartRotationX, Mathf.Clamp(Body.transform.localEulerAngles.y + direction.x, 45, 135), 0);
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void LateUpdate()
     {
-        if (other.CompareTag("Glacier"))
-        {
-            if (!other.GetComponent<Glacier>().IsActive)
-            {
-                if (nextSlot == 0)
-                {
-                    nextSlot = 1;
-                }
-                else
-                {
-                    nextSlot = 0;
-                }
-                other.GetComponent<Glacier>().ActivateObject(EmptySlots[nextSlot].transform);
+        Arm1.transform.localRotation = new Quaternion(0, 0, Arm1.transform.localRotation.z, Arm1.transform.localRotation.w);
+        Arm2.transform.localRotation = new Quaternion(0, 0, Arm2.transform.localRotation.z, Arm2.transform.localRotation.w);
+        Arm3.transform.localRotation = new Quaternion(0, 0, Arm3.transform.localRotation.z, Arm3.transform.localRotation.w);
+        Arm4.transform.localRotation = new Quaternion(0, 0, Arm4.transform.localRotation.z, Arm4.transform.localRotation.w);
+        CheckGround();
+    }
 
-                StartCoroutine(MovePlayerForward());
-            }
+    #region
+    [SerializeField] Transform LeftBack, RightBack;
+    RaycastHit hit;
+    bool isFalled;
+
+    public void CheckGround()
+    {
+        if (isFalled)
+        {
+            return;
+        }
+        if (Physics.Raycast(LeftBack.position, Vector3.down, out hit, 1))
+        {
+            Debug.DrawRay(LeftBack.position, Vector3.down * hit.distance, Color.blue);
+        }
+        else
+        {
+            Debug.DrawRay(LeftBack.position, Vector3.down * 1, Color.red);
+            Debug.Log("Fall Left");
+            isFalled = true;
+            //Fall Left
+        }
+
+        if (Physics.Raycast(RightBack.position, Vector3.down, out hit, 1))
+        {
+            Debug.DrawRay(RightBack.position, Vector3.down * hit.distance, Color.blue);
+        }
+        else
+        {
+            Debug.DrawRay(RightBack.position, Vector3.down * 1, Color.red);
+            Debug.Log("Fall Right");
+            isFalled = true;
+            //Fall Right
         }
     }
-
-    IEnumerator MovePlayerForward()
-    {
-        yield return new WaitForSeconds(.1f);
-        Debug.Log("Moveforward");
-        GlacierParent.transform.position -= new Vector3(0, 0, (Vector3.Distance(EmptySlots[0].transform.position, EmptySlots[1].transform.position) / 2));
-        EmptySlots[nextSlot].transform.position += new Vector3(0, 0, (Vector3.Distance(EmptySlots[0].transform.position, EmptySlots[1].transform.position)));
-    }
+    #endregion
 }
---
+++ Assets/Scripts/CollisionDetection.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+    public GameObject GlacierParent;
+    int nextSlot = 1;
+    public GameObject[] EmptySlots;   
+
+    IEnumerator MovePlayerForward()
+    {
+        yield return new WaitForSeconds(.1f);
+        Debug.Log("Moveforward");
+        GlacierParent.transform.position -= new Vector3(0, 0, (Vector3.Distance(EmptySlots[0].transform.position, EmptySlots[1].transform.position) / 2));
+        EmptySlots[nextSlot].transform.position += new Vector3(0, 0, (Vector3.Distance(EmptySlots[0].transform.position, EmptySlots[1].transform.position)));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Glacier"))
+        {
+            if (!other.GetComponent<Glacier>().IsActive)
+            {
+                if (nextSlot == 0)
+                {
+                    nextSlot = 1;
+                }
+                else
+                {
+                    nextSlot = 0;
+                }
+                other.GetComponent<Glacier>().ActivateObject(EmptySlots[nextSlot].transform);
+
+                StartCoroutine(MovePlayerForward());
+            }
+        }
+    }
+}
