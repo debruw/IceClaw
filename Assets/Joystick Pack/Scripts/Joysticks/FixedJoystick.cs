67c495f502b25e18d1b013ab88b45130d88fed40
Ebru Çakar <ebru_cakar_11@hotmail.com>
2021-05-13 02:49:31 +0300

    Ufak tefek ayarlamalar.

--- Assets/Joystick Pack/Scripts/Joysticks/FixedJoystick.cs
+++ Assets/Joystick Pack/Scripts/Joysticks/FixedJoystick.cs
@@ -1,8 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class FixedJoystick : Joystick
 {
+    protected override void Start()
+    {
+        base.Start();
+        background.gameObject.SetActive(false);
+    }
 
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        background.gameObject.SetActive(true);
+        base.OnPointerDown(eventData);
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        background.gameObject.SetActive(false);
+        base.OnPointerUp(eventData);
+    }
 }
